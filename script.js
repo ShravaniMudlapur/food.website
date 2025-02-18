2ed486476c1143ade4e32a8c6bc7dd0d231b49bb
ShravaniMudlapur <142003204+ShravaniMudlapur@users.noreply.github.com>
2025-02-18 16:50:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+});
+
+function orderNow() {
+    alert("Order placed successfully! 🍔🍕");
+}
